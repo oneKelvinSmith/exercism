6e752b156399fe104ebc5c27bb42ddd462381136
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-02-02 16:40:48 +0000

    rust: implement leap with gold plating

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,19 @@
+trait DivisibleBy {
+    fn is_divisible_by(self, num: i32) -> bool;
+    fn is_not_divisible_by(self, num: i32) -> bool;
+}
+
+impl DivisibleBy for i32 {
+    fn is_divisible_by(self, num: i32) -> bool {
+        self % num == 0
+    }
+    fn is_not_divisible_by(self, num: i32) -> bool {
+        !self.is_divisible_by(num)
+    }
+}
+
 pub fn is_leap_year(year: i32) -> bool {
-    year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    year.is_divisible_by(4) &&
+        year.is_not_divisible_by(100) ||
+        year.is_divisible_by(400)
 }
