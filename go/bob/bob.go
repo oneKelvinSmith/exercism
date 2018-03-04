275d1853b8e8e0e7f909887d997393f2ec6755b1
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-04 16:58:37 +0000

    go: first successful attempt at bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,29 +1,53 @@
 package bob
 
 import (
-	"fmt"
 	"strings"
+	"unicode"
 )
+
+const (
+	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
+// Remark is a convenience type for identifying what kind of remark it is.
+type Remark struct {
+	remark string
+}
+
+func (r Remark) isSilence() bool {
+	return strings.TrimSpace(r.remark) == ""
+}
+
+func (r Remark) isShouting() bool {
+	trimmed := strings.TrimFunc(r.remark, func(char rune) bool {
+		return unicode.IsDigit(char) ||
+			unicode.IsPunct(char) ||
+			unicode.IsSpace(char)
+	})
+
+	return strings.ToUpper(trimmed) == trimmed &&
+		strings.ContainsAny(r.remark, upper)
+}
+
+func (r Remark) isQuestion() bool {
+	trimmed := strings.TrimSpace(r.remark)
+	return strings.HasSuffix(trimmed, "?")
+}
 
 // Hey greets with an appropriate remark.
 func Hey(remark string) string {
-	fmt.Printf("Remark: %v\n", remark)
-
-	shouting := func() bool {
-		return strings.ToUpper(remark) == remark
-	}
-
-	questioning := func() bool {
-		return strings.HasSuffix(remark, "?")
-	}
-
+	r := Remark{remark}
 	switch {
-	case shouting() && questioning():
-		return "Calm down, I know what I'm doing!"
-	case shouting():
+	case r.isSilence():
+		return "Fine. Be that way!"
+	case r.isShouting():
+		if r.isQuestion() {
+			return "Calm down, I know what I'm doing!"
+		}
 		return "Whoa, chill out!"
-	case questioning():
+	case r.isQuestion():
 		return "Sure."
+
 	default:
 		return "Whatever."
 	}
