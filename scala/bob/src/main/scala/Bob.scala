9fa0a50d78e58f6da4edbf44fffce690e2f39fe9
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-05 17:51:54 +0000

    scala: bob now uses boolean extractor objects

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,58 +1,34 @@
 object Bob {
   case class Remark(statement: String) {
-    def isShouting = {
+    def isQuestioning: Boolean = statement.endsWith("?")
+    def isShouting: Boolean = {
       statement.indexWhere(char => char.isLetter) >= 0 &&
         statement.toUpperCase == statement
     }
-    def isQuestioning = statement.endsWith("?")
-    def isSilence     = statement.isEmpty
-  }
-
-  object Shout {
-    def unapply(remark: Remark): Option[String] = {
-      if (remark.isShouting) {
-        Some("Whoa, chill out!")
-      } else {
-        None
-      }
-    }
-  }
-
-  object Question {
-    def unapply(remark: Remark): Option[String] = {
-      if (remark.isQuestioning) {
-        Some("Sure.")
-      } else {
-        None
-      }
-    }
+    def isSilence: Boolean = statement.trim.isEmpty
   }
 
   object ForcefulQuestion {
-    def unapply(remark: Remark): Option[String] = {
-      if (remark.isQuestioning && remark.isShouting) {
-        Some("Calm down, I know what I'm doing!")
-      } else {
-        None
-      }
-    }
+    def unapply(remark: Remark): Boolean = remark.isQuestioning && remark.isShouting
+  }
+
+  object Question {
+    def unapply(remark: Remark): Boolean = remark.isQuestioning
+  }
+
+  object Shout {
+    def unapply(remark: Remark): Boolean = remark.isShouting
   }
 
   object Silence {
-    def unapply(remark: Remark): Option[String] = {
-      if (remark.isSilence) {
-        Some("Fine. Be that way!")
-      } else {
-        None
-      }
-    }
+    def unapply(remark: Remark): Boolean = remark.isSilence
   }
 
   def response(statement: String): String = Remark(statement.trim) match {
-    case ForcefulQuestion(reply) => reply
-    case Question(reply)         => reply
-    case Shout(reply)            => reply
-    case Silence(reply)          => reply
-    case _                       => "Whatever."
+    case ForcefulQuestion() => "Calm down, I know what I'm doing!"
+    case Question()         => "Sure."
+    case Shout()            => "Whoa, chill out!"
+    case Silence()          => "Fine. Be that way!"
+    case _                  => "Whatever."
   }
 }
