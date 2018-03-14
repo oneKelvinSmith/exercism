5217a210ab3f8a47d108d8533b576dd3429c50e4
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-14 19:45:01 +0000

    go: implement two-fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,9 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package twofer
 
-// ShareWith should have a comment documenting it.
-func ShareWith(string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+// ShareWith returns a string containing the name of a person to share with.
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
 }
