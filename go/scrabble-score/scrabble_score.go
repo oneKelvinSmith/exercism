d5c0f8531c4b822a9826c4231a46449e88a43391
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-15 21:19:37 +0000

    go: scrabble-score remove unnecessary casts

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -4,44 +4,44 @@ import (
 	"unicode"
 )
 
-var characterScores = map[rune]int{
-	rune('a'): 1,
-	rune('b'): 3,
-	rune('c'): 3,
-	rune('d'): 2,
-	rune('e'): 1,
-	rune('f'): 4,
-	rune('g'): 2,
-	rune('h'): 4,
-	rune('i'): 1,
-	rune('j'): 8,
-	rune('k'): 5,
-	rune('l'): 1,
-	rune('m'): 3,
-	rune('n'): 1,
-	rune('o'): 1,
-	rune('p'): 3,
-	rune('q'): 10,
-	rune('r'): 1,
-	rune('s'): 1,
-	rune('t'): 1,
-	rune('u'): 1,
-	rune('v'): 4,
-	rune('w'): 4,
-	rune('x'): 8,
-	rune('y'): 4,
-	rune('z'): 10,
+var values = map[rune]int{
+	'a': 1,
+	'b': 3,
+	'c': 3,
+	'd': 2,
+	'e': 1,
+	'f': 4,
+	'g': 2,
+	'h': 4,
+	'i': 1,
+	'j': 8,
+	'k': 5,
+	'l': 1,
+	'm': 3,
+	'n': 1,
+	'o': 1,
+	'p': 3,
+	'q': 10,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'u': 1,
+	'v': 4,
+	'w': 4,
+	'x': 8,
+	'y': 4,
+	'z': 10,
 }
 
 // Score calculates an returns the scrabble score for a given word.
 func Score(word string) int {
 	score := 0
-	for _, character := range word {
-		score += scoreFor(character)
+	for _, letter := range word {
+		score += valueFor(letter)
 	}
 	return score
 }
 
-func scoreFor(character rune) int {
-	return characterScores[unicode.ToLower(character)]
+func valueFor(letter rune) int {
+	return values[unicode.ToLower(letter)]
 }
