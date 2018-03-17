bfa2b4271b45c5337f8cf856fe12f10b768ec06d
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-17 17:34:34 +0000

    go: use sort in isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,24 +1,48 @@
 package isogram
 
 import (
+	"sort"
 	"unicode"
 )
 
 // IsIsogram determines if a given string is a word without repeating letters.
 func IsIsogram(word string) bool {
-	letters := make(map[rune]int)
+	letters := filter(word)
+	letters.reverseSort()
 
-	for _, char := range word {
-		if unicode.IsLetter(char) {
-			letter := unicode.ToLower(char)
-
-			if letters[letter] != 0 {
-				return false
-			}
-
-			letters[letter]++
+	for index := 0; index < len(letters)-1; index++ {
+		if letters[index] == letters[index+1] {
+			return false
 		}
 	}
 
 	return true
+}
+
+func filter(word string) sortable {
+	letters := []rune{}
+	for _, letter := range word {
+		if unicode.IsLetter(letter) {
+			letters = append(letters, unicode.ToLower(letter))
+		}
+	}
+	return sortable(letters)
+}
+
+func (letters sortable) reverseSort() {
+	sort.Sort(letters)
+}
+
+type sortable []rune
+
+func (letters sortable) Less(left, right int) bool {
+	return letters[left] > letters[right]
+}
+
+func (letters sortable) Swap(left, right int) {
+	letters[left], letters[right] = letters[right], letters[left]
+}
+
+func (letters sortable) Len() int {
+	return len(letters)
 }
