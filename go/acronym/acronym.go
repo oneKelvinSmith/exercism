078b2a746d75cf5c9e359744b4c1755f9d1c398f
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-18 16:07:31 +0000

    go: simplify accronym based on feedback

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -4,17 +4,12 @@ import (
 	"unicode"
 )
 
-const (
-	space  = rune(32)
-	hyphen = rune(45)
-)
-
 // Abbreviate creates acronyms from a string.
 func Abbreviate(s string) string {
 	acronymRunes := []rune{}
-	previousRune := space
-	for _, r := range []rune(s) {
-		if previousRune == space || previousRune == hyphen {
+	previousRune := ' '
+	for _, r := range s {
+		if unicode.IsSpace(previousRune) || previousRune == '-' {
 			acronymRunes = append(acronymRunes, unicode.ToUpper(r))
 		}
 		previousRune = r
