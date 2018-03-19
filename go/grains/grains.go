894f2f67b5301f3633283d94e28e599e708ea9b8
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-19 16:09:48 +0000

    go: use bitshifting in grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -3,28 +3,19 @@ package grains
 import "errors"
 
 // Square returns the number of grains of wheat on a chess board for a given chess square.
-func Square(number int) (count uint64, err error) {
-	if number <= 0 || 64 < number {
+func Square(position int) (number uint64, err error) {
+	if position <= 0 || 64 < position {
 		err = errors.New("invalid square")
 
 		return
 	}
 
-	count = square(number)
+	number = 1 << uint64(position-1)
 
 	return
 }
 
-func square(number int) uint64 {
-	if number == 1 {
-		return 1
-	}
-
-	return 2 * square(number-1)
-
-}
-
 // Total returns the total amount of grain a chessboard represents.
 func Total() uint64 {
-	return 18446744073709551615
+	return 1<<64 - 1
 }
