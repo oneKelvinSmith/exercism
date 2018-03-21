dcdeb2f4f110f58e67fc1d4af5c5e8321efccd58
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-21 17:30:00 +0000

    go: combine files in parallel-letter-frequency

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+import "sync"
+
 type FreqMap map[rune]int
 
 func Frequency(s string) FreqMap {
@@ -8,4 +10,36 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+// ConcurrentFrequency returns the number of each unicode letter in a group of texts.
+func ConcurrentFrequency(texts []string) FreqMap {
+	frequencies := &concurrentFreqMap{freqMap: FreqMap{}}
+	channel := make(chan *concurrentFreqMap)
+
+	for _, text := range texts {
+		go channelledFrequency(text, frequencies, channel)
+	}
+
+	for range texts {
+		<-channel
+	}
+
+	return frequencies.freqMap
+}
+
+type concurrentFreqMap struct {
+	sync.RWMutex
+	freqMap FreqMap
+}
+
+func channelledFrequency(text string, frequencies *concurrentFreqMap, channel chan *concurrentFreqMap) {
+	frequencies.Lock()
+	defer frequencies.Unlock()
+
+	for _, letter := range text {
+		frequencies.freqMap[letter]++
+	}
+
+	channel <- frequencies
 }
