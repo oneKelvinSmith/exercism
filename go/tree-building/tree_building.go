4f7c542ab75747aa98d4712097de8fee2e785a5b
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-24 13:48:34 +0000

    go: tree-building do it all in one loop

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -10,62 +10,34 @@ type Record struct {
 	ID, Parent int
 }
 
-func (record *Record) isRoot() bool {
-	return record.ID == 0
-}
-func (record *Record) isChild() bool {
-	return !record.isRoot()
-}
-func (record *Record) isChildOf(node *Node) bool {
-	return record.isChild() && record.Parent == node.ID
-}
-
 // Node represents a component of a tree.
 type Node struct {
 	ID       int
 	Children []*Node
 }
 
-func (node *Node) build(records []Record, size int) {
-	if size == len(records) {
-		return
-	}
-	size++
-
-	for _, record := range records {
-		if record.isChildOf(node) {
-			child := &Node{ID: record.ID}
-			child.build(records, size)
-
-			node.Children = append(node.Children, child)
-		}
-	}
-}
-
-func reorder(records []Record) {
+func reorder(records []Record) []Record {
 	sort.Slice(records, func(i, j int) bool {
 		return records[i].ID < records[j].ID
 	})
+
+	return records
 }
 
-func validate(records []Record) (err error) {
-	for index, record := range records {
-		if index >= 1 && record.ID == records[index-1].ID {
-			return fmt.Errorf("duplicate record found")
-		}
-		if record.ID >= len(records) {
-			return fmt.Errorf("records not contiguous")
-		}
-		if record.ID < record.Parent {
-			return fmt.Errorf("indirect cycle detected")
-		}
-		if record.isChild() && record.ID == record.Parent {
-			return fmt.Errorf("cycle detected")
-		}
-		if record.isRoot() && record.Parent != 0 {
-			return fmt.Errorf("root has a parent")
-		}
+func validate(record Record, nodes []*Node) (err error) {
+	switch {
+	case record.ID == 0 && record.Parent != 0:
+		return fmt.Errorf("root has a parent")
+	case record.ID < record.Parent:
+		return fmt.Errorf("indirect cycle detected")
+	case record.ID != 0 && record.ID == record.Parent:
+		return fmt.Errorf("cycle detected")
+	case record.ID >= len(nodes):
+		return fmt.Errorf("records not contiguous")
+	case nodes[record.ID] != nil:
+		return fmt.Errorf("duplicate record found")
 	}
+
 	return
 }
 
@@ -75,15 +47,19 @@ func Build(records []Record) (root *Node, err error) {
 		return
 	}
 
-	err = validate(records)
-	if err != nil {
-		return nil, err
+	nodes := make([]*Node, len(records))
+	for index, record := range reorder(records) {
+		if err := validate(record, nodes); err != nil {
+			return nil, err
+		}
+
+		nodes[index] = &Node{ID: record.ID}
+		if index > 0 {
+			parent := nodes[record.Parent]
+			parent.Children = append(parent.Children, nodes[index])
+		}
 	}
-
-	reorder(records)
-
-	root = &Node{}
-	root.build(records, 1)
+	root = nodes[0]
 
 	return
 }
