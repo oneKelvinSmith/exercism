b066c1fc332d0b19dd273f34b56f85c9e36c0050
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-27 14:27:08 +0100

    go: difference-of-squares refactoring

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -5,16 +5,18 @@ func SquareOfSums(n int) int {
 	sum := arithmeticProgression(n)
 	return sum * sum
 }
-func arithmeticProgression(n int) int {
-	return n * (n + 1) / 2
-}
 
 // SumOfSquares retuns the sum of squares for the first n natural numbers.
 func SumOfSquares(n int) int {
 	return faulhaber(n)
 }
+
 func faulhaber(n int) int {
-	return n * (n + 1) * (2*n + 1) / 6
+	return arithmeticProgression(n) * (2*n + 1) / 3
+}
+
+func arithmeticProgression(n int) int {
+	return n * (n + 1) / 2
 }
 
 // Difference returns the difference between the and SquareOfSums
