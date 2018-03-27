d99c6dda2450afd008231bf7020431c408841fc5
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-03-27 14:22:04 +0100

    go: difference-of-squares with arithmetic progression

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,18 +2,17 @@ package diffsquares
 
 // SquareOfSums retuns the square of sums for the first n natural numbers.
 func SquareOfSums(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
+	sum := arithmeticProgression(n)
 	return sum * sum
+}
+func arithmeticProgression(n int) int {
+	return n * (n + 1) / 2
 }
 
 // SumOfSquares retuns the sum of squares for the first n natural numbers.
 func SumOfSquares(n int) int {
 	return faulhaber(n)
 }
-
 func faulhaber(n int) int {
 	return n * (n + 1) * (2*n + 1) / 6
 }
