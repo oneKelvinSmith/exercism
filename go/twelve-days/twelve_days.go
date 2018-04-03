abf03d95f7b4476ba754b50ed77e2b01e11d6448
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-04-03 14:27:48 +0100

    go: twelve-days second pass

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -6,56 +6,60 @@ import (
 
 const verseCount = 12
 
-var components = [verseCount][3]string{
-	{"first", "a", "Partridge in a Pear Tree"},
-	{"second", "two", "Turtle Doves"},
-	{"third", "three", "French Hens"},
-	{"fourth", "four", "Calling Birds"},
-	{"fifth", "five", "Gold Rings"},
-	{"sixth", "six", "Geese-a-Laying"},
-	{"seventh", "seven", "Swans-a-Swimming"},
-	{"eighth", "eight", "Maids-a-Milking"},
-	{"ninth", "nine", "Ladies Dancing"},
-	{"tenth", "ten", "Lords-a-Leaping"},
-	{"eleventh", "eleven", "Pipers Piping"},
-	{"twelfth", "twelve", "Drummers Drumming"},
+type verse struct {
+	day  string
+	gift string
 }
 
+func (v *verse) gifts(index int) (gifts string) {
+	if index == 0 {
+		return v.gift
+	}
+
+	gifts = v.gift
+
+	seperator := ", "
+	for index--; index >= 0; index-- {
+		if index == 0 {
+			seperator = ", and "
+		}
+		gifts += seperator + verses[index].gift
+	}
+
+	return
+}
+
+var verses = [verseCount]*verse{
+	{"first", "a Partridge in a Pear Tree"},
+	{"second", "two Turtle Doves"},
+	{"third", "three French Hens"},
+	{"fourth", "four Calling Birds"},
+	{"fifth", "five Gold Rings"},
+	{"sixth", "six Geese-a-Laying"},
+	{"seventh", "seven Swans-a-Swimming"},
+	{"eighth", "eight Maids-a-Milking"},
+	{"ninth", "nine Ladies Dancing"},
+	{"tenth", "ten Lords-a-Leaping"},
+	{"eleventh", "eleven Pipers Piping"},
+	{"twelfth", "twelve Drummers Drumming"},
+}
+
+// Song returns all twelve verses of the song.
 func Song() (song string) {
 	for number := 1; number <= verseCount; number++ {
 		song += Verse(number) + "\n"
 	}
+
 	return
 }
 
+// Verse return the nth verse of the song.
 func Verse(number int) (verse string) {
+	index := number - 1
 	verse = fmt.Sprintf(
 		"On the %s day of Christmas my true love gave to me, %s.",
-		day(number-1), accumulatedGifts(number-1),
+		verses[index].day, verses[index].gifts(index),
 	)
-
-	return
-}
-
-func day(index int) string {
-	return components[index][0]
-}
-
-func accumulatedGifts(index int) (gifts string) {
-	gifts = components[index][1] + " " + components[index][2]
-
-	if index == 0 {
-		return
-	}
-
-	seperator := ", "
-	if index == 1 {
-		seperator = ", and "
-	}
-
-	index--
-
-	gifts += seperator + accumulatedGifts(index)
 
 	return
 }
