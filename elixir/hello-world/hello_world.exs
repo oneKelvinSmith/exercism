57abf3df76e656b97e4234f121ad0879521c63b1
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-08-06 10:11:05 +0100

    elixir: hello-world

--- elixir/hello-world/hello_world.exs
+++ elixir/hello-world/hello_world.exs
@@ -1,31 +1,12 @@
 defmodule HelloWorld do
   @moduledoc """
-  Elixir counts the number of arguments as part of the function name.
-  For instance;
-
-      def hello() do
-      end
-
-  would be a completely different function from
-
-      def hello(name) do
-      end
-
-  Can you find a way to make all the tests pass with just one
-  function?
-
-  Hint: look into argument defaults here:
-  http://elixir-lang.org/getting-started/modules.html#default-arguments
+  A module to say hello to the world.
   """
 
   @doc """
   Greets the user by name, or by saying "Hello, World!"
   if no name is given.
   """
-  @spec hello(String.t) :: String.t
-  def hello(name) do
-
-    "Your implementation goes here"
-
-  end
+  @spec hello(String.t()) :: String.t()
+  def hello(name \\ "World") when is_binary(name), do: "Hello, #{name}!"
 end
--- elixir/hello-world/hello_world_test.exs
+++ elixir/hello-world/hello_world_test.exs
@@ -2,8 +2,8 @@ if !System.get_env("EXERCISM_TEST_EXAMPLES") do
   Code.load_file("hello_world.exs")
 end
 
-ExUnit.start
-ExUnit.configure exclude: :pending, trace: true
+ExUnit.start()
+ExUnit.configure(exclude: :pending, trace: true)
 
 defmodule HelloWorldTest do
   use ExUnit.Case
@@ -12,14 +12,11 @@ defmodule HelloWorldTest do
     assert HelloWorld.hello() == "Hello, World!"
   end
 
-  @tag :pending
   test "says hello sample name" do
     assert HelloWorld.hello("Alice") == "Hello, Alice!"
   end
 
-  @tag :pending
   test "says hello other sample name" do
     assert HelloWorld.hello("Bob") == "Hello, Bob!"
   end
-
 end
