1f1f7bf3e2206c87f6825872d20891fd7d8987a1
Kelvin Smith <oneKelvinSmith@gmail.com>
2019-03-28 20:23:42 +0000

    typescript: hello-world use default name

--- typescript/hello-world/hello-world.ts
+++ typescript/hello-world/hello-world.ts
@@ -1,10 +1,6 @@
 class HelloWorld {
-  static hello(name?: string): string {
-    if (name) {
-      return `Hello, ${name}!`
-    }
-
-    return 'Hello, World!'
+  static hello(name: string = "World"): string {
+    return `Hello, ${name}!`
   }
 }
 
