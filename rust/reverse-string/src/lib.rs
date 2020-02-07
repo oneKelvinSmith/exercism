67db7d24fe53669c028cd737e71173cbb4f99af6
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-02-07 15:47:29 +0000

    rust: reverse-string use one-liner

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,5 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut characters: Vec<char> = input.chars().collect();
-    characters.reverse();
-    characters.iter().collect()
+    input.chars().rev().collect()
 }
