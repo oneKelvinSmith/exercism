3b508b8ee25d061e4caa9eb5dcc8e07a9a1faf4c
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-02-24 16:58:35 +0000

    rust: reverse-string specify return type

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -2,5 +2,5 @@ extern crate unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    input.graphemes(true).rev().collect()
+    input.graphemes(true).rev().collect::<String>()
 }
