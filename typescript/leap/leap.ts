1a3d405794c640980ea3995bb67f67c04b01b426
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-03-12 09:34:54 +0000

    typescript: leap try get order of operation really right

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,9 +1,9 @@
 function isLeapYear(year: number): boolean {
   const divisibleBy = (base: number, divisor: number) => {
-    return base % divisor === 0
+    return !(base % divisor)
   }
 
-  return divisibleBy(year, 400) || !divisibleBy(year, 100) && divisibleBy(year, 4)
+  return (divisibleBy(year, 4) && !divisibleBy(year, 100)) || divisibleBy(year, 400)
 }
 
 export default isLeapYear
