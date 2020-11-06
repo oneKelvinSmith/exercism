8dea5e4f9656423096df6cca07180259d16900b8
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-11-06 21:14:54 +0000

    clojure: binary-search initial attempt

--- clojure/binary-search/src/binary_search.clj
+++ clojure/binary-search/src/binary_search.clj
@@ -1,9 +1,22 @@
 (ns binary-search)
 
-(defn search-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn spy [tag value] (prn tag value) value)
 
-(defn middle [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn middle [collection]
+  (quot (count collection) 2))
+
+(defn search-for
+  ([item collection]
+   (loop [items collection
+          index 0]
+     (let [middle-index (middle items)
+           middle-item (get items middle-index)
+           items-to-left (into [] (take middle-index items))
+           items-to-right (into [] (drop (+ middle-index 1) items))]
+
+       (cond
+         (< item middle-item) (recur items-to-left index)
+         (> item middle-item) (recur items-to-right (+ 1 index middle-index))
+         :else (+ index middle-index))
+       )))
+  )
