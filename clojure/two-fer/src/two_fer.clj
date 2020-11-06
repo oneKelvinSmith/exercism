e1b44b47fc76d09661ec897825ffa33f0c976177
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-11-06 15:41:31 +0000

    clojure: two-fer first pass

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,5 +1,5 @@
 (ns two-fer)
 
-(defn two-fer [name] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn two-fer
+  ([] "One for you, one for me.")
+  ([name] (str "One for " name ", one for me.")))
