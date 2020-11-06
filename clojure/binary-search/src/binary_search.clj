e21ca8652d7d806c6821f399dbdb3c265f150fa6
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-11-06 21:36:29 +0000

    clojure: binary-search using `vec`

--- clojure/binary-search/src/binary_search.clj
+++ clojure/binary-search/src/binary_search.clj
@@ -7,19 +7,19 @@
 
 (defn search-for
   [item collection]
-  (loop [items (spy "collection" (into [] collection))
+  (loop [items (vec collection)
          start-index 0]
     (if (empty? items) (throw (Exception. "not found")))
 
     (let [middle-index (middle items)
           middle-item (get items middle-index)
-          items-to-left (into [] (take middle-index items))
-          items-to-right (into [] (drop (+ middle-index 1) items))]
+          items-to-left (vec (take middle-index items))
+          items-to-right (vec (drop (+ middle-index 1) items))]
 
       (cond
-        (= item middle-item) (spy "equal" (+ start-index middle-index))
-        (< item middle-item) (recur (spy "left" items-to-left) start-index)
-        (> item middle-item) (recur (spy "right" items-to-right) (+ 1 start-index middle-index))
+        (= item middle-item) (+ start-index middle-index)
+        (< item middle-item) (recur items-to-left start-index)
+        (> item middle-item) (recur items-to-right (+ 1 start-index middle-index))
         )
       )
     )
