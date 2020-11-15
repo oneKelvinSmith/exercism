79c7fd58b703716d67a577ab2b09dc522fd99916
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-11-15 22:30:38 +0000

    clojure: bob first pass

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,19 @@
-(ns bob)
+(ns bob,
+  (:require [clojure.string :as string]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- silence? [remark] (string/blank? remark))
+(defn- sensible? [remark] (not (nil? (re-find #"[A-z]" remark))))
+(defn- question? [remark] (string/ends-with? remark "?"))
+(defn- forceful? [remark] (and (sensible? remark) (= remark (string/upper-case remark))))
+(defn- exclamation? [remark] (and (forceful? remark) (question? remark)))
+
+(defn response-for [s]
+  (let [remark (string/trim s)]
+    (cond
+      (silence? remark) "Fine. Be that way!"
+      (exclamation? remark) "Calm down, I know what I'm doing!"
+      (question? remark) "Sure."
+      (forceful? remark) "Whoa, chill out!"
+      :else "Whatever."
+      ))
+  )
