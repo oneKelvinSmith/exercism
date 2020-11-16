92a471971b5f410ddac67f5cefe475dc9cedccb3
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-11-16 10:23:05 +0000

    clojure: bob using `let` and `apply` with `condp`

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,19 +1,19 @@
 (ns bob,
   (:require [clojure.string :as string]))
 
-(defn- silence? [remark] (string/blank? remark))
-(defn- sensible? [remark] (not (nil? (re-find #"[A-z]" remark))))
-(defn- question? [remark] (string/ends-with? remark "?"))
-(defn- forceful? [remark] (and (sensible? remark) (= remark (string/upper-case remark))))
-(defn- exclamation? [remark] (and (forceful? remark) (question? remark)))
-
 (defn response-for [s]
-  (let [remark (string/trim s)]
-    (cond
-      (silence? remark) "Fine. Be that way!"
-      (exclamation? remark) "Calm down, I know what I'm doing!"
-      (question? remark) "Sure."
-      (forceful? remark) "Whoa, chill out!"
-      :else "Whatever."
+  (let [remark (string/trim s)
+        question? #(string/ends-with? % "?")
+        sensible? #(not (nil? (re-find #"[A-z]" %)))
+        forceful? #(and (sensible? %) (= % (string/upper-case %)))
+        exasperated? #(and (forceful? %) (question? %))
+        silence? #(string/blank? %)]
+
+    (condp apply [remark]
+      silence? "Fine. Be that way!"
+      exasperated? "Calm down, I know what I'm doing!"
+      forceful? "Whoa, chill out!"
+      question? "Sure."
+      "Whatever."
       ))
   )
