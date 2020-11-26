399c6bd552df42455bf12445527a793f59adb6b8
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-11-26 17:29:44 +0000

    clojure: armstrong-numbers first pass

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,17 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- digits
+  ([number]
+   (digits number ()))
+  ([n acc]
+   (if (zero? n)
+     acc
+     (digits (quot n 10) (conj acc (rem n 10))))))
+
+(defn- exp [base exponent] (reduce * (repeat exponent base)))
+
+(defn- strongarm [number]
+  (let [ds (digits number)]
+    (reduce + (map #(exp % (count ds)) ds))))
+
+(defn armstrong? [number] (= number (strongarm number)))
