83a33b5a5af34b18b0b90031de52aebd16d56e73
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-12-18 11:23:50 +0000

    clojure: rna-transcription first pass

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,12 @@
-(ns rna-transcription)
+(ns rna-transcription
+  (:require [clojure.string :as string]))
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn to-rna [dna]
+  (let [validate #(if (= (count %) (count dna))
+                    %
+                    (throw (AssertionError. (str "Invalid DNA: " dna))))
+        dna->rna {\C \G
+                  \G \C
+                  \A \U
+                  \T \A}]
+    (validate (string/join (map dna->rna dna)))))
