1873e48960ed8072a79cc31c2be7aba1fe96ebe6
Kelvin Smith <oneKelvinSmith@gmail.com>
2021-05-05 12:25:50 +0100

    clojure: rna-subscription all tests passing

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -2,11 +2,11 @@
   (:require [clojure.string :as string]))
 
 (defn to-rna [dna]
-  (let [validate #(if (= (count %) (count dna))
-                    %
-                    (throw (AssertionError. (str "Invalid DNA: " dna))))
-        dna->rna {\C \G
-                  \G \C
-                  \A \U
-                  \T \A}]
-    (validate (string/join (map dna->rna dna)))))
+  (let [mapping {\C \G
+                 \G \C
+                 \A \U
+                 \T \A}
+        dna->rna #(if-let [[_ rna] (find mapping %)]
+                    rna
+                    (throw (AssertionError. (str "Invalid DNA: " dna))))]
+    (string/join (map dna->rna dna))))
