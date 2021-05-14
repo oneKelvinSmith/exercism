67abfc004437483a2098d42ecbc184384c841b77
Kelvin Smith <oneKelvinSmith@gmail.com>
2021-05-14 14:14:52 +0100

    clojure: rna-transcription use `assert`

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -2,11 +2,9 @@
   (:require [clojure.string :as string]))
 
 (defn to-rna [dna]
-  (let [mapping {\C \G
-                 \G \C
-                 \A \U
-                 \T \A}
-        dna->rna #(if-let [[_ rna] (find mapping %)]
-                    rna
-                    (throw (AssertionError. (str "Invalid DNA: " dna))))]
-    (string/join (map dna->rna dna))))
+  (let [lookup-rna #({\C \G \G \C \A \U \T \A} %)
+        dna->rna #(string/join (map lookup-rna %))
+        rna (dna->rna dna)]
+    (assert (= (count rna) (count dna)) (str "Invalid DNA: " dna))
+    rna))
+
