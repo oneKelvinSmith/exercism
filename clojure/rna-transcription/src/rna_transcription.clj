453ff02fe3464bf897e2e67f60efe8be3f8bc388
Kelvin Smith <oneKelvinSmith@gmail.com>
2021-05-19 16:11:37 +0100

    clojure: rna-transcription appease exercism reviewer

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,8 +1,9 @@
 (ns rna-transcription
   (:require [clojure.string :as string]))
 
+(def dna->rna {\C \G \G \C \A \U \T \A})
 (defn to-rna [dna]
-  (let [rna (#(string/join (map {\C \G \G \C \A \U \T \A} %)) dna)]
+  (let [rna (string/join (map dna->rna dna))]
     (assert (= (count rna) (count dna)) (str "Invalid DNA: " dna))
     rna))
 
