570da30bc550fff32dab49847282ce5f4720b259
Kelvin Smith <oneKelvinSmith@gmail.com>
2021-05-24 12:00:33 +0100

    java: implement scrabble-score

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,24 @@
 class Scrabble {
 
+    private final int score;
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.score = word.codePoints().map(this::characterScore).sum();
     }
 
-    int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int getScore() {
+        return score;
     }
 
+    private int characterScore(int c) {
+        return switch (Character.toLowerCase((char) c)) {
+            case 'q', 'z' -> 10;
+            case 'j', 'x' -> 8;
+            case 'k' -> 5;
+            case 'f', 'h', 'v', 'w', 'y' -> 4;
+            case 'b', 'c', 'm', 'p' -> 3;
+            case 'd', 'g' -> 2;
+            default -> 1;
+        };
+    }
 }
