d198cb7035fa0181248fba463d951fad3650b07c
Kelvin Smith <oneKelvinSmith@gmail.com>
2021-06-02 09:26:12 +0100

    clojure: run-length-encoding implement `encode`

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -2,7 +2,14 @@
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [plain-text])
+  [plain-text]
+  (let [character-groups (partition-by identity plain-text)
+        group-encoder (fn [encoded group]
+                        (let [character (first group)
+                              frequency (count group)
+                              code (if (= 1 frequency) "" frequency)]
+                          (str encoded code character)))]
+    (reduce group-encoder "" character-groups)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
